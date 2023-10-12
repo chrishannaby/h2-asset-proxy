d29d951703b245f5092db35d23bd52ecc26c6cc7
chrishannaby <chrishannaby@gmail.com>
2023-10-12 11:58:00 -0600

    add cdn proxy for font

---
+++ app/routes/[.]cdn.$.tsx
@@ -0,0 +1,15 @@
+import {type LoaderArgs} from '@shopify/remix-oxygen';
+
+export async function loader({request, context}: LoaderArgs) {
+  const {oxygenAssetsUrl} = context;
+  const {pathname, search} = new URL(request.url);
+  const proxyPath = pathname.replace('/.cdn/', '');
+  const proxyUrl = `${oxygenAssetsUrl}/${proxyPath}${search}`;
+  const proxyResponse = await fetch(proxyUrl, {
+    headers: request.headers,
+  });
+  return new Response(proxyResponse.body, {
+    status: proxyResponse.status,
+    headers: new Headers(proxyResponse.headers),
+  });
+}
